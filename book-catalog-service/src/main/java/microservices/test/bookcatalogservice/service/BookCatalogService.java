52be8b83372eaa6ab66db717106e0dd4252c25bf
Aveek Majumdar <aveek.majumdar123456@gmail.com>
2020-11-10 15:10:25 +0530

    ...

--- book-catalog-service/src/main/java/microservices/test/bookcatalogservice/service/BookCatalogService.java
+++ book-catalog-service/src/main/java/microservices/test/bookcatalogservice/service/BookCatalogService.java
@@ -1,6 +1,7 @@
 package microservices.test.bookcatalogservice.service;
 
 import java.util.List;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import microservices.test.bookcatalogservice.model.Book;
 import microservices.test.bookcatalogservice.model.BookCatalog;
 import microservices.test.bookcatalogservice.model.BookRatings;
+import reactor.core.publisher.Mono;
 
 @Service
 public class BookCatalogService {
@@ -19,11 +21,11 @@ public class BookCatalogService {
 
 	public List<BookCatalog> getBookCatalogs(Integer userId) {
 
-		BookRatings ratings = webClient.build().get().uri("http://localhost:8083/rating/" + userId).retrieve()
-				.bodyToMono(BookRatings.class).block();
+		Mono<BookRatings> ratings = webClient.build().get().uri("http://localhost:8083/rating/" + userId).retrieve()
+				.bodyToMono(BookRatings.class);
 		 
 		
-		return ratings.getRatings().stream().map(rating -> {
+		return ratings.block().getRatings().stream().map(rating -> {
 			Book book = webClient.build().get().uri("http://localhost:8082/master/" + rating.getBookId()).retrieve()
 					.bodyToMono(Book.class).block();
 			return new BookCatalog(book.getBookId(), userId, book.getBookName(), rating.getRating());
